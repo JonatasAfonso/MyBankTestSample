f039aebe4ff74371cd312ef4653075418129b124
Jônatas Afonso Oliveira Neto <jonatas.oliveira.neto@fidelidade.pt>
2021-11-03 13:07:03 +0000

    Publish

---
+++ MyBank/MyBankLib/CheckingAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyBankLib
+{
+    public class CheckingAccount : IAccount
+    {
+        private double m_balance;
+        private string name;
+        private double amount;
+
+        public CheckingAccount(string name, double amount)
+        {
+            this.name = name;
+            this.amount = amount;
+        }
+
+        public double Balance { get; set; }
+
+        public void Withdraw(double amount)
+        {
+            if (m_balance >= amount)
+            {
+                m_balance -= amount;
+            }
+            else
+            {
+                throw new ArgumentException("Withdrawal exceeds balance!", nameof(amount));
+            }
+        }
+    }
+}
